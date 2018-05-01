165e6d90a8fcf82174ce3bd68127395ca20f11f5
Omar <rpachecosantos0@saddleback.edu>
2018-05-01 11:50:01 -0700

    add testimonialwindow.h

---
+++ testimonialwindow.h
@@ -0,0 +1,22 @@
+#ifndef TESTIMONIALWINDOW_H
+#define TESTIMONIALWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class testimonialWindow;
+}
+
+class testimonialWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit testimonialWindow(QWidget *parent = 0);
+    ~testimonialWindow();
+
+private:
+    Ui::testimonialWindow *ui;
+};
+
+#endif // TESTIMONIALWINDOW_H
